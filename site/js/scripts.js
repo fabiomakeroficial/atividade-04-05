fd638dd70be2fe40b6ce2f0f4181be655dcb2c7f
devfabio <fabiohgoulart9@gmail.com>
2026-07-19 01:49:42 -0300

    feat: :package: integração com a API ViaCEP para autopreenchimento de endereço

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -1,3 +1,35 @@
+// API ViaCEP (Autopreenchimento)
+document.getElementById('cep').addEventListener('blur', function () {
+    const cep = this.value.replace(/\D/g, '');
+
+    if (cep.length === 8) {
+        const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (!data.erro) {
+
+                    document.getElementById('endereco').value = `${data.logradouro}, ${data.bairro} - ${data.localidade}/${data.uf}`;
+                } else {
+                    alert('CEP não encontrado.');
+                    limparEndereco();
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao buscar o CEP:', error);
+                alert('Erro ao buscar o CEP. Tente novamente.');
+            });
+    } else if (cep.length > 0) {
+        alert('CEP inválido. Digite exatamente 8 números.');
+        limparEndereco();
+    }
+});
+
+function limparEndereco() {
+    document.getElementById('endereco').value = '';
+}
+
 // Botão "Agende uma consulta gratuita"
 document.getElementById('btnBanner').addEventListener('click', function () {
 
